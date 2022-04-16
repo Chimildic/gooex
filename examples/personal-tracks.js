72cd56625ad45e214107e0d27323ecf8cd89bfa9
Chimildic <40138097+Chimildic@users.noreply.github.com>
2022-04-16 12:25:26 +0300

    Update example personal tracks

--- examples/personal-tracks.js
+++ examples/personal-tracks.js
@@ -1,15 +1,20 @@
-/** gooex build 2022.04.15
+/** gooex build 2022.04.16
  * Собрать треки из персональных плейлистов (Тайник, Дежавю и т.д.)
  */
 function updatePersonalPlaylist() {
-    let kit = gooex.Context.getKit(undefined, 2000, 20);
+  let kit = gooex.Context.getKit(undefined, 2000, 20);
 
-    let tracks = gooex.Landing.getPersonalTracks();
-    gooex.Filter.removeTracks(tracks, kit.all);
+  let includeTypes = [
+    'recentTracks', 'neverHeard', 'origin', 'playlistOfTheDay'
+  ]; // podcasts, family, kinopoisk
 
-    gooex.Playlist.saveWithReplace({
-        name: 'Персональные треки',
-        tracks: tracks,
-        description: 'Собрано из персональных плейлистов',
-    });
+  let tracks = gooex.Landing.getPersonalTracks(...includeTypes);
+  gooex.Filter.removeTracks(tracks, kit.all);
+  gooex.Filter.match(tracks, '^[a-zA-Z0-9 ]+$'); // только латиница
+
+  gooex.Playlist.saveWithReplace({
+    name: 'Персональные треки',
+    tracks: tracks,
+    description: 'Собрано из персональных плейлистов',
+  });
 }
